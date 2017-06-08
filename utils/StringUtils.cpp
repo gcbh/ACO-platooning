2cca33aa8a7a07e6fc98af7cdebcd07f23d86d03
Sean Amadio <samadio@uwaterloo.ca>
2017-06-08 14:31:57 -0400

    Added initial edge class and string utils for parsing

---
+++ utils/StringUtils.cpp
@@ -0,0 +1,26 @@
+//
+//  StringUtils.cpp
+//  
+//
+//  Created by Sean Amadio on 2017-06-06.
+//
+//
+
+#include "StringUtils.hpp"
+
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
---
+++ utils/StringUtils.hpp
@@ -0,0 +1,20 @@
+//
+//  StringUtils.hpp
+//  
+//
+//  Created by Sean Amadio on 2017-06-06.
+//
+//
+
+#ifndef StringUtils_hpp
+#define StringUtils_hpp
+
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+
+std::vector<std::string> split(const std::string &s, char delim);
+
+#endif /* StringUtils_hpp */
