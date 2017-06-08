0ca39917acf4285eebc4d7da16faa8dc30827c46
PRIYA BIBRA <p2bibra@uwaterloo.ca>
2017-06-07 23:46:18 -0400

    initial commit to dijkstra

--- transport_layer/main.cpp
+++ transport_layer/main.cpp
@@ -8,7 +8,23 @@
 
 #include <stdio.h>
 #include "ACO.h"
+#include <iostream>
+#include <ifstream>
+#include <map>
+
+std::map<int, t_node> graph;
 
 int main() {
+    ifstream myReadFile;
+    myReadFile.open("cities_p.txt");
+    
+    if (myReadFile.is_open()) {
+        while (!myReadFile.eof()) {
+            myReadFile >> output;
+            cout<<output;
+            
+        }
+    }
+    myReadFile.close();
     return 0;
 }
